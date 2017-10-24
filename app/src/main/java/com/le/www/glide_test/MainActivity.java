060b58ddced8836e8b629a10c48a2c93aa981774
wangweijun1 <wangweijun1@le.com>
2017-10-24 16:20:56 +0800

    use jar

--- app/src/main/java/com/le/www/glide_test/MainActivity.java
+++ app/src/main/java/com/le/www/glide_test/MainActivity.java
@@ -2,6 +2,9 @@ package com.le.www.glide_test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends Activity {
 
@@ -10,10 +13,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-//        Glide.with(MainActivity.this)
-//                .load("myUrl")
-//                .into(imageView);
+        ImageView imageView = (ImageView)findViewById(R.id.iv);
+        Glide.with(MainActivity.this)
+                .load("http://i1.letvimg.com//lc03_iptv//201706//13//13//50//tmp10cf626e-4099-4b3b-99a9-b6e709de7ac8256.png")
+                .into(imageView);
 
     }
 }
